a7cc9307cfd35757b0880aa1cb80075bc18df46d
aduis2 <aduis2@LAPTOP-5L26ID4E>
2020-12-21 20:16:41 +0800

    asd

--- src/git/TestofGit.java
+++ src/git/TestofGit.java
@@ -4,7 +4,7 @@ public class TestofGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-asd
+
 	}
 
 }
--- src/package1/TestofGit_1.java
+++ src/package1/TestofGit_1.java
@@ -4,7 +4,7 @@ public class TestofGit_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+qwe
 	}
 
 }
